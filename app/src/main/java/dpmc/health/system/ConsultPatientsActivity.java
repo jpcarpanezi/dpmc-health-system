16556e7a0122cac5675c8442c6403f8e53272e2b
João Pedro Carpanezi dos Santos <jpcarpanezi@gmail.com>
2021-12-15 16:49:56 -0300

    Added CardView to person informations

--- app/src/main/java/dpmc/health/system/ConsultPatientsActivity.java
+++ app/src/main/java/dpmc/health/system/ConsultPatientsActivity.java
@@ -1,19 +1,23 @@
 package dpmc.health.system;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NavUtils;
 
 import android.os.Bundle;
 import android.text.InputType;
 import android.text.TextWatcher;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.Locale;
+import java.util.Optional;
 
 import dpmc.health.system.interfaces.MedicationsClient;
 import dpmc.health.system.interfaces.PersonClient;
@@ -31,6 +35,16 @@ public class ConsultPatientsActivity extends AppCompatActivity {
 
     private TextWatcher textWatcher = null;
 
+    private TextView name;
+    private TextView birthDate;
+    private TextView email;
+    private TextView phone;
+    private TextView bloodType;
+    private TextView medicalConditions;
+    private TextView allergies;
+    private TextView comments;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -38,11 +52,28 @@ public class ConsultPatientsActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setTitle(R.string.title_consult_patients);
 
+        this.name = findViewById(R.id.name);
+        this.birthDate = findViewById(R.id.birthDate);
+        this.email = findViewById(R.id.email);
+        this.phone = findViewById(R.id.phone);
+        this.bloodType = findViewById(R.id.bloodType);
+        this.medicalConditions = findViewById(R.id.medicalConditions);
+        this.allergies = findViewById(R.id.allergies);
+        this.comments = findViewById(R.id.comments);
+
+        this.name.setText(getString(R.string.person_name, ""));
+        this.birthDate.setText(getString(R.string.person_birth_date, ""));
+        this.email.setText(getString(R.string.person_email, ""));
+        this.phone.setText(getString(R.string.person_phone, ""));
+        this.bloodType.setText(getString(R.string.person_blood_type, ""));
+        this.medicalConditions.setText(getString(R.string.person_medical_conditions, ""));
+        this.allergies.setText(getString(R.string.person_allergies, ""));
+        this.comments.setText(getString(R.string.person_comments, ""));
+
         this.retrofitBuild();
 
         Spinner spinner = (Spinner) findViewById(R.id.spinner);
         spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener(){
-
             @Override
             public void onItemSelected(AdapterView<?> arg0, View arg1,
                                        int arg2, long arg3) {
@@ -84,7 +115,18 @@ public class ConsultPatientsActivity extends AppCompatActivity {
             public void onNothingSelected(AdapterView<?> arg0) {
             }
         });
+    }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            // Respond to the action bar's Up/Home button
+            case android.R.id.home:
+                NavUtils.navigateUpFromSameTask(this);
+
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     public void searchButtonClick(View view) {
@@ -110,6 +152,14 @@ public class ConsultPatientsActivity extends AppCompatActivity {
                     InputMethodManager imm = (InputMethodManager) getSystemService(INPUT_METHOD_SERVICE);
                     imm.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(), 0);
 
+                    name.setText(getString(R.string.person_name, person.getPersonName()));
+                    birthDate.setText(getString(R.string.person_birth_date, person.getBirthDate()));
+                    email.setText(getString(R.string.person_email, person.getEmail()));
+                    phone.setText(getString(R.string.person_phone, person.getPhone()));
+                    bloodType.setText(getString(R.string.person_blood_type, Optional.ofNullable(person.getMedicalInformations().getBloodType()).orElse("Sem dados")));
+                    medicalConditions.setText(getString(R.string.person_medical_conditions, Optional.ofNullable(person.getMedicalInformations().getMedicalConditions()).orElse("Sem dados")));
+                    allergies.setText(getString(R.string.person_allergies, Optional.ofNullable(person.getMedicalInformations().getAllergies()).orElse("Sem dados")));
+                    comments.setText(getString(R.string.person_comments, Optional.ofNullable(person.getMedicalInformations().getObservations()).orElse("Sem dados")));
                 } else {
                     Toast.makeText(getApplicationContext(), "A pessoa não foi encontrada", Toast.LENGTH_LONG).show();
                 }
